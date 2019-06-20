ec673b01eac62c1851cd3855e15bb7708bd9b4bd
ryancnelson <ryan@ryan.net>
2019-06-20 00:54:45 -0400

    Updated

--- se-workshop/setup.sh
+++ se-workshop/setup.sh
@@ -4,15 +4,15 @@ echo Waiting for initial setup ...
   do
     sleep 1
   done
+sleep 3
 chmod 777 /var/run/screen
 
-# git clone https://github.com/ryancnelson/spiffe-example
-git clone https://github.com/ryancnelson/spiffe-example-workshop
-cd spiffe-example-workshop/dupin
-make
+##git clone https://github.com/ryancnelson/spiffe-example-workshop
+##cd spiffe-example-workshop/dupin
+##make
 
-export SPIRE_GOOPTS=""
-export DEBUG=""
+##export SPIRE_GOOPTS=""
+##export DEBUG=""
 chmod 777 /var/run/screen
 echo 'environment setup.  Have fun! '
 
